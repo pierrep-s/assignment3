fed44c81cbf303d8e3570f391f24c1718f41fb63
Pierre PAC SOO <ppacso@trusty>
2016-03-24 12:59:17 -0400

    adding all

---
+++ parse.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include<stdbool.h>
+
+#define BOOLEAN int
+#define TRUE 1
+#define FALSE 0
+
+
+void initBuffer(char *inputLine){
+
+}
+
+
+BOOLEAN hasNextToken(){
+	
+}
+
+
+
+char *nextToken(){
+
+}
+
+
+void rewind(){
+
+}
